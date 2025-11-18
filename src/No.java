9d6076b2d07b53715af82783d984d2bb49ed62a5
João Paulo Gobira <jpgobira2@gmail.com>
2025-11-18 07:48:12 -0300

    create: Nós da árvore

---
+++ src/No.java
@@ -0,0 +1,49 @@
+package src;
+
+public class No<K,V> {
+    private K chave;
+    private V item;
+    private No<K,V> esq;
+    private No<K,V> dir;
+    
+    public No(K chave, V item){
+        this.chave=chave;
+        this.item=item;
+        esq=null;
+        dir=null;
+    }
+
+    public K getChave() {
+        return chave;
+    }
+
+    public void setChave(K chave) {
+        this.chave = chave;
+    }
+
+    public V getItem() {
+        return item;
+    }
+
+    public void setItem(V item) {
+        this.item = item;
+    }
+
+    public No<K, V> getEsq() {
+        return esq;
+    }
+
+    public void setEsq(No<K, V> esq) {
+        this.esq = esq;
+    }
+
+    public No<K, V> getDir() {
+        return dir;
+    }
+
+    public void setDir(No<K, V> dir) {
+        this.dir = dir;
+    }
+
+
+}
