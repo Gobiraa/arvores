846008360ca03a2d9a448eae06c5e2d45881bd45
João Paulo Gobira <jpgobira2@gmail.com>
2025-12-06 18:26:52 -0300

    add: interface mapeamento

---
+++ src/IMapeamento.java
@@ -0,0 +1,10 @@
+package src;
+
+public interface IMapeamento<K,V> {
+
+    public int inserir(K chave, V item);
+    public V pesquisar(K chave);
+    public V remover(K chave);
+    public int tamanho();
+    public String percorrer();
+}
